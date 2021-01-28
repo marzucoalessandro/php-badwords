ac8a1e401ae6e8f6303382632aceb266fca7c47a
Alessandro <marzucoalessandro1@gmail.com>
2021-01-28 19:40:48 +0100

    fine esercizio

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
   $_GET["name"];
+  $name = $_GET["name"];
   $string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
  ?>
@@ -12,7 +13,8 @@
   <body>
     <h1> <?php echo $string; ?></h1>
     <h2> <?php echo strlen($string) ?></h2>
-    <h3> <?php echo str_replace("amet","***",$string) ?></h3>
+    <h2> <?php echo $name ?></h2>
+    <h2> <?php echo str_replace("$name","***",$string) ?></h2>
 
 
   </body>
